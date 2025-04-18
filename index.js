35e502bba8de0343cf9936a7df84076ed1e2ec50
Ahmed Shobky <zashobky4@gmail.com>
2025-04-19 00:00:08 +0200

    new edits

---
+++ index.js
@@ -0,0 +1,17 @@
+// server.js
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/hello' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, world sss!');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
